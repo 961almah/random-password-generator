3eaa73d796ef8e81c354b657c1485c0200f3bedd
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-01 20:22:04 -0400

    modified the appraoch

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", prompts);
 
 // Write password to the #password input
 function writePassword() {
@@ -8,82 +9,73 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+function prompts() {
+  alert(
+    "Hello! This is a random password generator. Please choose in the following boxes what options you wish to include in your password."
+  );
+  if (confirm(lowercaseOption) === true) {
+    lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  } else {
+    lowerCaseLetters = "";
+  }
+
+  console.log(lowercaseOption);
+  console.log(lowerCaseLetters);
+}
+
 // create arrays and variables for content usage
-var characters = [
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "+",
-  ",",
-  "-",
-  ".",
-  "/",
-  ":",
-  ";",
-  "<",
-  "=",
-  "<",
-  ">",
-  "?",
-  "@",
-  "[",
-  "]",
-  '"',
-  "^",
-  "_",
-  "`",
-  "{",
-  "|",
-  "}",
-  "~",
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+var numbers = "0123456789";
+
+var uppercaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-// Create prompt to greet and give details
+
+// Create an alert to greet and give details
+
 // create prompts asking about inclusion of lowercase, uppercase, numeric, and/or special characters
+var lengthOption =
+  "How long would you like your password to be? Choose a number between 8 and 128 for the total number of characters.";
+var lowercaseOption =
+  "Would you like your passowrd to include lowercase characters?";
+var uppercaseOption =
+  "Would you like your password to include uppercase characters?";
+var numericOption =
+  "Would you like your password to include numeric characters?";
+var specialCharactersOption =
+  "Would you like your password to include special characters?";
+
+// Create if statements
+
 // create push to store commits inside variable
 // create randomizer
 // create a loop
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
