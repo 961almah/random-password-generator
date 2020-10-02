ee795a370d63939ed7f6512ce5f7875103535113
Ali Mahmoud <961alimahmoud@gmail.com>
2020-10-01 22:48:46 -0400

    rephrased pseudocodes

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Assignment Code
+// Assignment Code.
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", prompts);
 
-// Write password to the #password input
+// Write password to the #password input.
 
 function prompts() {
-  yourPassword = "";
-  // Create an alert to greet and give details
+  // Create an alert to greet and give details.
   alert(
     "Hello! This is a random password generator. Please choose in the following boxes what options you wish to include in your password."
   );
 
-  // create prompts asking about inclusion of lowercase, uppercase, numeric, and/or special characters
-
+  // create preliminary variables.
   var lowercaseOption =
     "Would you like your passowrd to include lowercase characters?";
   var uppercaseOption =
@@ -22,54 +20,67 @@ function prompts() {
   var specialCharactersOption =
     "Would you like your password to include special characters?";
 
-  // create if statement to determine value of lower case letters
-
+  // create prompt to ask user to choose a number of characters.
   var lengthChosen = prompt(
     "How long would you like your password to be? Choose a number between 8 and 128 for the total number of characters."
   );
+  // Make it halt process if the number of characters is not between 8-128.
   if (lengthChosen < 8 || lengthChosen > 128) {
     alert("Please choose a number between 8 and 128.");
+
+    // If number meets requirements, ask user if they want to include lower case letters.
   } else {
     if (confirm(lowercaseOption) === true) {
       var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-    } else {
+    }
+    // if they choose not to, characters won't be included.
+    else {
       var lowerCaseLetters = "";
     }
     console.log(lowerCaseLetters);
 
-    // create if statement to determine value of upper case letters
+    // ask user if they want to include upper case letters.
     if (confirm(uppercaseOption) === true) {
       var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    } else {
+    }
+    // if they choose not to, characters won't be included.
+    else {
       var uppercaseLetters = "";
     }
     console.log(uppercaseLetters);
 
-    // create statement to determine value of numbers
+    // ask user if they want to include lower case letters.
     if (confirm(numericOption) === true) {
       var numbers = "123456789";
-    } else {
+    }
+    // if they choose not to, characters won't be included.
+    else {
       var numbers = "";
     }
     console.log(numbers);
 
-    // create if statment to determine value of special characters
+    // ask user if they want to include lower case letters.
     if (confirm(specialCharactersOption) === true) {
       var specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-    } else {
+    }
+    // if they choose not to, characters won't be included.
+    else {
       var specialCharacters = "";
     }
     console.log(specialCharacters);
   }
-
+  // consolidate chosen options into one
   chosenOptions = lowerCaseLetters.concat(
     uppercaseLetters,
     numbers,
     specialCharacters
   );
 
+  yourPassword = "";
+
   console.log(chosenOptions);
 
+  // create loop and randomizer
   for (let i = 0; i < lengthChosen; i++) {
     yourPassword += chosenOptions.charAt(
       Math.floor(Math.random() * chosenOptions.length)
